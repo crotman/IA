0e2a4f97d99b255bf5a149f9a4ab6021f261586e
crotman <crotman@gmail.com>
2018-09-03 00:01:13 -0300

    Add files via upload

--- CalculaIntegral.py
+++ CalculaIntegral.py
@@ -76,6 +76,29 @@ class Transformacoes_Finais(Transformacoes):
 
         self.transformacoes.append(Transformacao(reconhece_x_elevado_n, calcula_x_elevado_n,funcao_identidade,"x elevado a constante"))
 
+        def reconhece_constante(expressao):
+            expressao_eh_constante = isinstance(expressao,sympy.numbers.Number)
+            return expressao_eh_constante
+
+        def calcula_constante(expressao):
+            return 0
+
+        self.transformacoes.append(Transformacao(reconhece_constante, calcula_constante, funcao_identidade, "constante"))
+
+        def reconhece_exponencial(expressao):
+            expressao_eh_exponencial = expressao.func == exp
+            expoente_eh_x = isinstance(expressao.args[0],sympy.Symbol)
+            return expressao_eh_exponencial and expoente_eh_x
+
+        def calcula_exponencial(expressao):
+            return expressao
+
+        self.transformacoes.append(Transformacao(reconhece_exponencial, calcula_exponencial, funcao_identidade,"exponencial"))
+
+
+
+
+
 class Transformacoes_Certeiras(Transformacoes):
 
     transformacoes = list()
@@ -112,6 +135,12 @@ class Transformacoes_Certeiras(Transformacoes):
 
         self.transformacoes.append(Transformacao(reconhece_constante_multiplicando_funcao_com_simbolo, retira_constante_multiplicada, multiplica_constante_de_volta, "integral(cx) = c integral(x)"))
 
+
+        #def reconhece_divisao_polinomios(expressao):
+        #    eh_mult = expressao.func == 
+
+
+
 class Transformacoes_Heuristicas(Transformacoes):
 
     transformacoes = list()
@@ -120,18 +149,43 @@ class Transformacoes_Heuristicas(Transformacoes):
 
     def __init__(self):
         
-        """Ainda não tem nada"""
-        def reconhece_soma(expressao):
-            expressao_eh_soma = isinstance(expressao,sympy.Add)
-            return expressao_eh_soma
-                
-        def separa_parcelas_soma(expressao):
-            return (list(expressao.args))
-        
-        def soma_parcelas(lista_expressoes):
-            return(sum(lista_expressoes))
 
-        self.transformacoes.append(Transformacao(reconhece_soma, separa_parcelas_soma, soma_parcelas, "integral da soma é a soma das integrais"))
+        def operador_eh_sen_expoente_positivo_ou_cos_expoente_negativo(operador):
+            expressao_eh_potenciacao = isinstance(operador,sympy.Pow)
+            if (operador.args[0].func == cos):
+                eh_simbolo = isinstance(operador.args[0].args[0], sympy.Symbol)
+                expoente_correto = isinstance(operador.args[1],sympy.numbers.Number) and operador.args[1] < 0
+            else:
+                if (operador.args[0].func == sin):
+                    eh_simbolo = isinstance(operador.args[0].args[0], sympy.Symbol)
+                    expoente_correto = isinstance(operador.args[1],sympy.numbers.Number) and operador.args[1] > 0
+                else:
+                    return false
+            return expressao_eh_potenciacao and eh_simbolo and expoente_correto
+
+        def reconhece_sin_sobre_cos(expressao):
+            try:
+                expressao_eh_multiplicacao = isinstance(expressao,sympy.Mul)
+                operador1_eh_ok = operador_eh_sen_expoente_positivo_ou_cos_expoente_negativo(expressao.args[0])
+                operador2_eh_ok = operador_eh_sen_expoente_positivo_ou_cos_expoente_negativo(expressao.args[1])
+                n_operadores_eh_2 = len(expressao.args) == 2            
+                expoentes_iguais = abs(expressao.args[0].args[1]) == abs(expressao.args[1].args[1])
+                return expressao_eh_multiplicacao and operador1_eh_ok and operador2_eh_ok and n_operadores_eh_2 and expoentes_iguais 
+            except:
+                return false
+                
+        def transforma_em_tangente(expressao):
+            expoente = abs(expressao.args[0].args[1])
+            simbolo = expressao.args[0].args[0].args[0]
+            return [(tan(simbolo)**expoente)]
+        
+        def funcao_identidade(expressao_base,lista_expressoes_resultado):
+            return lista_expressoes_resultado[0]
+
+        self.transformacoes.append(Transformacao(reconhece_sin_sobre_cos, transforma_em_tangente, funcao_identidade, "sen/cos vira tan"))
+
+
+
 
 
 transformacoes_finais = Transformacoes_Finais()
@@ -224,16 +278,13 @@ x = symbols('x')
 
 
 
-expressao = x**2 + x**3
+expressao = sin(x)**4 / cos(x)**4
 no = No(expressao, None)
 expressao = no.resolve_depth()
 print("Resultado:")
 print(expressao)
 print("Árvore")
 print(no)
-
-
-
 
 
 
